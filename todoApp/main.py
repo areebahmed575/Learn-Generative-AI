24fedada10c0928e8bdc9de846e0b4abcfb56ac9
areebahmed575 <areebahmed575@gmail.com>
2024-01-26 02:31:51 +0500

    Todo App completed

--- todoApp/main.py
+++ todoApp/main.py
@@ -9,12 +9,29 @@ app = FastAPI()
 db = SessionLocal()
 
 class OurBaseModel(BaseModel):
-    class Config:
-        from_attributes = True
+    class Config: # Config class is used to configure various settings for a model.
+        from_attributes = True 
 
 class Todo(OurBaseModel):
     message: str
     status: bool
+
+@app.get('/', status_code=status.HTTP_200_OK)
+def getAll_Todo():
+    # Get all Todos from the database
+    getAllTodo = db.query(models.Todo).all()
+    return getAllTodo
+
+
+@app.get('/getbyid/{todo_id}', response_model=Todo, status_code=status.HTTP_200_OK)
+def get_Single_Todo(todo_id: int):
+    # Get a single Todo from the database by their ID
+    get_Todos = db.query(models.Todo).filter(
+        models.Todo.id == todo_id).first()
+    if get_Todos is not None:
+        return get_Todos
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Todo not found")
 
 @app.post('/addTodo', response_model=Todo, status_code=status.HTTP_201_CREATED)
 def addTodo(todo: Todo):
@@ -24,6 +41,17 @@ def addTodo(todo: Todo):
 
     return newTodo
 
+@app.put('/update_todo/{todo_id}', status_code=status.HTTP_202_ACCEPTED)
+def update_Todo(todo_id: int, todo: Todo):
+    find_Todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if find_Todo is not None:
+        find_Todo.message = todo.message
+        find_Todo.status = todo.status
+
+        db.commit()
+        return find_Todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Todo not found")
 
 @app.delete('/delete_todo/{todo_id}', response_model=Todo, status_code=200)
 def delete_Todo(todo_id: int):
@@ -35,3 +63,14 @@ def delete_Todo(todo_id: int):
         return find_Todo
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail="Todo not found")
+
+
+
+# status_code= status.HTTP_202_ACCEPTED: This status code indicates that the request has been accepted for processing, but the processing has not been completed yet. It's often used for asynchronous processing,
+# status_code= status.HTTP_201_CREATED: This status code indicates that the request has been successfully fulfilled and a new resource has been created as a result of it. It's commonly used in POST requests where a new resource is created on the server based on the data provided in the request body.
+# Use status_code=200 when the request has been successfully processed, and you want to return the requested resource or indicate a generic success response.
+
+
+
+
+
--- todoApp/streamlit_client.py
+++ todoApp/streamlit_client.py
@@ -9,26 +9,73 @@ st.title("Todo App")
 def createtodo():
     st.markdown("Create Todo")      
     title = st.text_input(
-        "Enter New Todo", placeholder="Read Book before Sleep") 
+        "Enter New Todo", placeholder="Paying bills") 
     convert_str = str(title)  
     if st.button("Add Todo"):  
         if title: 
             response = requests.post(f"{BASE_URL}/addTodo", json={"message": convert_str, "status": False})
             st.success("Added Todo Successfully")  
 
+def update_todo():
+    st.markdown("Update Todo")  
+    todo_id = st.number_input("Enter Todo ID to Edit",step=1, min_value=0) 
+    message = st.text_input("Enter Todo", placeholder="Updating financial records")
+    status = st.radio("Enter Status", ('False', 'True'))  
+    status = True if status == 'True' else False 
 
+    data = {
+        "id": todo_id,
+        "message": message,
+        "status": status
+    }  
+
+    if st.button("Edit Todo"): 
+        url = f"http://127.0.0.1:8000/update_todo/{todo_id}"
+        response = requests.put(url, json=data)  
+        if response:  
+            st.success("Todo Updated Successfully")  
+        else:  
+            st.error(
+                f"PUT request failed with status code: {response.status_code}")  
+            print(response.text)
+
+
+
+    
 
 
 def delete_todo():
     st.markdown("Delete Todo")
     todo_id = st.number_input(
-        "Enter Todo ID to delete", step=1)  
-    todo_id = int(todo_id)  
-    if st.button("Delete Todo"):  
+        "Enter Todo ID to delete", step=1, min_value=0)  # Set min_value to 0
+    todo_id = int(todo_id)
+    if st.button("Delete Todo"):
         response = requests.delete(f"{BASE_URL}/delete_todo/{todo_id}")
-        st.warning("Deleted Todo Succesfully!")
-            
+        st.warning("Deleted Todo Successfully!")
+
+         
+
+def show_todo(): 
+    st.markdown("Here is your Todos List")
+    
+    response = requests.get("http://127.0.0.1:8000/")
+    todos = response.json()
+    table_data = []
+
+    for todo in todos:
+        table_data.append([todo['id'], todo['message'], todo['status']])
+
+    # Add header row
+    table_data.insert(0, ['id', 'message', 'status'])
+   
+    st.table(table_data)
+
+
+
 
 if __name__ == "__main__":
     createtodo()
+    update_todo()
     delete_todo()
+    show_todo()
+   
