fd5f94f27fa569acd86b8b87a31c4c1bc025d690
areebahmed575 <areebahmed575@gmail.com>
2024-01-29 14:07:02 +0500

    gpt first commit

--- Api/4_pytest/tests/test_sample.py
+++ Api/4_pytest/tests/test_sample.py
@@ -4,6 +4,6 @@ def func(x):
 
 def test_answer():
     assert func(3) == 5
-# for individual run pytest test_class.py
+# for individual run pytest test_class.py or pytest or pytest -vv
 # if you want not to get additional information pytest -q test_class.py
 # pytest will run all files of the form test_*.py or *_test.py in the current directory and its subdirectories. More generally, it follows    
