730765a29093e8a4f3a3997fd914a252f4894c05
areebahmed575 <areebahmed575@gmail.com>
2024-04-01 01:15:30 +0500

    Fast-api with poetry

---
+++ poetry/fast-api/fast_api/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+@app.get("/piaic/")
+def piaic():
+    return {"message": "piaic"}
+
