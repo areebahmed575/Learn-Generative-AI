404f98b59603c359897b7dafac60207b6b318ed8
areebahmed575 <areebahmed575@gmail.com>
2024-08-24 21:18:51 +0500

    compound ai system added

--- crewai/agents.py
+++ crewai/agents.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 import os
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_MODEL_NAME"]="gpt-4-0125-preview"
+os.environ["OPENAI_MODEL_NAME"]="gpt-4o-mini"
 
 
 ## Create a senior blog content researcher
