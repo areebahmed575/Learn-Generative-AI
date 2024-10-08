605a1bcd150bddd40c68bc4fee9632750d6413fc
areebahmed575 <areebahmed575@gmail.com>
2024-10-08 23:48:27 +0500

    fixes

--- Project/AsKPakTour/backend/app/main.py
+++ Project/AsKPakTour/backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI( title="Weather API",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
